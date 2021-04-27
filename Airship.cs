f56133ac92cec154f0b00dec5ca3a6ea26f1a619
tarka <tarkaklavio@gmail.com>
2021-04-27 12:55:43 +0200

    The start of a bullet hell game copy

---
+++ Airship.cs
@@ -0,0 +1,185 @@
+using System;
+using System.Collections.Generic;
+
+namespace Airship
+{
+    abstract class Blueprint : SFML.Graphics.CircleShape
+    {
+        internal int width = 800;
+        internal int height = 1000;
+        protected int health;
+        protected int attack;
+        public SFML.Graphics.CircleShape airshipShape;
+        public class Bullets
+        {
+            public SFML.System.Vector2f Position{get; set;}
+            public SFML.Graphics.CircleShape bulletShape;
+            public bool thrown;
+            public Bullets(SFML.System.Vector2f shootAngle)
+            {
+                this.bulletShape = new SFML.Graphics.CircleShape(5)
+                {
+                    FillColor = SFML.Graphics.Color.White
+                };
+                this.thrown = false;
+                this.Position = new SFML.System.Vector2f(shootAngle.X + 35, shootAngle.Y - 20);
+                this.bulletShape.Position = this.Position;
+                
+            }
+            public void update(SFML.System.Vector2f vectorPosition)
+            {
+                {
+                    if (this.Position.Y - 10 >= 0)
+                    {
+                        this.Position = new SFML.System.Vector2f( vectorPosition.X + 35, this.Position.Y - 10);
+                        this.bulletShape.Position = this.Position;
+                    }
+                    else if (this.Position.Y <= 0)
+                    {
+                        this.thrown = false;
+                        this.Position = new SFML.System.Vector2f(vectorPosition.X + 35, vectorPosition.Y - 20);   
+                    }
+                    else if (this.Position.Y >= 1000)
+                    {
+                        this.thrown = false;
+                        this.Position = new SFML.System.Vector2f(vectorPosition.X + 35, vectorPosition.Y - 20); 
+                    }
+                    else if (this.Position.Y + 10 < 1000)
+                    {
+                        this.Position = new SFML.System.Vector2f( vectorPosition.X + 35, this.Position.Y + 10);
+                        this.bulletShape.Position = this.Position;
+                    }
+                }
+            }
+        }
+        public interface Skills{
+            int skill1();
+        }
+    }
+
+    class EnemyAirships : Blueprint, Blueprint.Skills
+    {
+        private System.Random random = new System.Random(); 
+        public int Health{
+            get{return this.health;}
+            set
+            {
+                this.health -= value;
+            }}
+        public EnemyAirships()
+        {
+             this.airshipShape = new SFML.Graphics.CircleShape(40, 3)
+            {
+                FillColor = SFML.Graphics.Color.Blue
+            };
+            this.Position = new SFML.System.Vector2f( width / 2 - 40, 100);
+            this.airshipShape.Position = this.Position;
+            this.airshipShape.Rotation = 180.0f;
+            this.Rotation = 180.0f;
+            this.health = 100;
+            this.attack = 20;
+        }
+        public void update(SFML.Graphics.RenderWindow window)
+        {
+            int decider = random.Next(2);
+            
+            if (decider == 0)
+            {
+                if (this.Position.X - 100 > 0)
+                {
+                    this.Position = new SFML.System.Vector2f( this.Position.X - 100, 100);
+                    this.airshipShape.Position = this.Position;
+                }
+                else if (this.Position.X - 100 <= 0)
+                {
+                    this.Position = new SFML.System.Vector2f(80, 100);
+                    this.airshipShape.Position = this.Position;
+                }
+            }
+            else if (decider == 1)
+            {
+                if (this.Position.X + 100 < 720)
+                {
+                    this.Position = new SFML.System.Vector2f( this.Position.X + 100, 100);
+                    this.airshipShape.Position = this.Position;
+                } 
+                else if (this.Position.X + 100 >= 720)
+                {
+                    this.Position = new SFML.System.Vector2f(780, 100);
+                    this.airshipShape.Position = this.Position;
+                } 
+                
+            }
+            
+        }
+
+        public int skill1(){
+
+            return 0;
+        }
+        
+    }
+    class Player : Blueprint, Blueprint.Skills, Update, MoveShip
+    {
+        private SFML.System.Vector2f movingDirection;
+        public int Score{get; set;}
+        public Player(float radius, uint pointCount)
+        {       
+            this.airshipShape = new SFML.Graphics.CircleShape(radius, pointCount)
+            {
+                FillColor = SFML.Graphics.Color.Blue
+            };
+            this.health = 100;
+            this.attack = 20;
+        }
+        public void update(SFML.System.Vector2f vector2D, SFML.Graphics.RenderWindow window) {
+                this.Position = vector2D;
+                this.airshipShape.Position = this.Position;       
+        }
+        public void moveShip(int direction){
+            if(direction == 0 )
+            {
+                movingDirection = new SFML.System.Vector2f(this.Position.X - 100.0f, this.Position.Y);
+                if (this.Position.X - 100 >= 0)
+                {
+                    this.Position = movingDirection;
+                }
+                else if (this.Position.X - 100 < 0)
+                {
+                    this.Position = new SFML.System.Vector2f(0, this.Position.Y);
+                }
+            }
+            else if (direction == 1)
+            {
+                movingDirection = new SFML.System.Vector2f(this.Position.X + 100.0f, this.Position.Y);
+                if (this.Position.X + 100<= 720)
+                {
+                    this.Position = movingDirection;
+                } 
+                else if (this.Position.X + 100 > 720)
+                {
+                    this.Position = new SFML.System.Vector2f(700, this.Position.Y);
+                } 
+            }
+        }
+        public int skill1()
+        {
+            return 20 * this.attack;
+        }
+    }
+
+    /// <summary>
+         /// Interfaces
+    /// </summary>
+    interface MoveShip{
+        void moveShip(int direction);
+    }
+    interface Update{
+        void update(SFML.System.Vector2f vector2D, SFML.Graphics.RenderWindow window);
+    }
+
+    interface blockPowerUp{
+        int attackUp();
+        int healthUp();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PixivAPI
+{
+    class Program
+    {
+       static void Main()
+       {
+           Console.WriteLine("Click Esc to close window");
+           var window = new SimpleWindow();
+           window.Run();
+
+           Console.WriteLine("All done!");
+       }
+    }
+    
+    class SimpleWindow
+    {
+        public Airship.Player player;
+        public List<Airship.EnemyAirships> enemyList;
+        public List<Airship.Blueprint.Bullets> bulletList;
+        public int count;
+        public void Run()
+        {
+            var window = new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(800, 1000), "SFML!");
+            window.KeyPressed += Window_KeyPressed;
+
+            player = new Airship.Player(40, 3);
+            player.Position = new SFML.System.Vector2f(window.Size.X / 2 - 40, 900);
+
+            enemyList = new List<Airship.EnemyAirships>();
+            bulletList = new List<Airship.Blueprint.Bullets>();
+            
+            for (int i = 0; i < 50; i++)
+            {
+                bulletList.Add(new Airship.Blueprint.Bullets(player.Position));
+            }
+
+            enemyList.Add(new Airship.EnemyAirships());
+            enemyList.Add(new Airship.EnemyAirships());
+            enemyList.Add(new Airship.EnemyAirships());
+            enemyList.Add(new Airship.EnemyAirships());
+
+
+            var scoreText = new SFML.Graphics.Text($"Score: {player.Score}", new SFML.Graphics.Font("myfont.ttf"));
+            while (window.IsOpen)
+            {
+                
+                window.Clear();
+                count += 1;
+                render(window, scoreText);
+                window.DispatchEvents();
+                
+                window.Display();
+            }
+        }
+        public void render(SFML.Graphics.RenderWindow window, SFML.Graphics.Text scoreText)
+        {
+            player.update(player.Position, window);
+            scoreText.DisplayedString = $"Score: {player.Score}";        
+            window.Draw(player.airshipShape);
+            foreach (Airship.EnemyAirships element in enemyList)
+            {
+                window.Draw(element.airshipShape);
+                if (count == 100){
+                    element.update(window);
+                    if (enemyList.IndexOf(element) == enemyList.Count - 1)
+                    {
+                        count = 0;
+                    }
+                }
+            }
+            for (int i = 0; i < bulletList.Count; i++)
+            {   
+                if (bulletList[i].thrown == true)
+                {
+                    window.Draw(bulletList[i].bulletShape); 
+                    bulletList[i].update(player.Position);
+                }
+                
+                for (int j = enemyList.Count - 1; j >= 0; j--)
+                {
+                    if (enemyList[j].Health <= 0)
+                    {
+                        enemyList.Remove(enemyList[j]);
+                    }
+                    if (bulletList[i].Position.X - 35 == enemyList[j].Position.X &&
+                        bulletList[i].Position.Y == enemyList[j].Position.Y )
+                    {
+                       enemyList[j].Health = 20; 
+                       bulletList[i].thrown = false;
+                       Console.WriteLine(enemyList[j].Health);
+                    }
+                }
+            }
+
+            window.Draw(scoreText);
+        }
+
+        /// <summary>
+            /// Function called when a key is pressed
+        /// </summary>
+        private void Window_KeyPressed(object sender, SFML.Window.KeyEventArgs e)
+        {
+            var window = (SFML.Window.Window)sender;
+            if (e.Code == SFML.Window.Keyboard.Key.Escape)
+            {
+                window.Close();
+            }
+            if(e.Code == SFML.Window.Keyboard.Key.Left)
+            {
+                player.moveShip(0);
+            }
+            if(e.Code == SFML.Window.Keyboard.Key.Right)
+            {
+                player.moveShip(1);
+            }
+            if(e.Code == SFML.Window.Keyboard.Key.Space)
+            {
+                foreach ( Airship.Blueprint.Bullets bullet in bulletList)
+                {
+                    if (bullet.thrown == false) 
+                    {
+                        bullet.thrown = true;
+                        break;
+                    }
+                }
+            }
+            
+        }
+    }
+
+}
